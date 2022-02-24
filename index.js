1fc98fefe2ba9e767a9b99eef1cda6f1219185a5
Rahul Shah <rashah1998@yahoo.com>
2022-02-23 19:09:59 -0500

    commit tree 21947d2cccac75b523a6de4505b327cb3c4d3d66

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr) {
+    return [arr[0], arr[1]];
+}
+
+const returnLastTwoDrivers = function(arr) {
+    return [arr[arr.length -2], arr[arr.length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return fare => fare*int;
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(arr, func) {
+    return func(arr);
+}
